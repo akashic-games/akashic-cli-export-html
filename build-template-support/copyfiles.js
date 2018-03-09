dcf8b4397753988dc5be75659c111e2a713c2d85
kamakiri01 <kamakiri01@users.noreply.github.com>
2018-03-09 14:11:59 +0900

    split template build package.json

--- build-template-support/copyfiles.js
+++ build-template-support/copyfiles.js
@@ -13,13 +13,13 @@ function minify(filepath) {
 };
 
 var files = [
-	"templates/template-export-html-v2/js/akashic-engine.js",
+	"js/akashic-engine.js",
 	"node_modules/@akashic/game-driver/build/game-driver.js",
 	"node_modules/@akashic/game-storage/build/game-storage.js",
 	"node_modules/@akashic/pdi-browser/build/pdi-browser.js"
 ];
 
 files.forEach(filepath => {
-	const outputPath = path.resolve("./templates/template-export-html-v2/js/", path.basename(filepath, ".js") + ".strip.js");
+	const outputPath = path.resolve(process.cwd(), "js", path.basename(filepath, ".js") + ".strip.js");
 	fs.writeFileSync(outputPath, minify(filepath).code);
 });
