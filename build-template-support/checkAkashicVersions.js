b99355d351d28ac42287cc9bd4cbd11973f80aa4
dera- <alchemy.and.fairy@gmail.com>
2018-06-14 18:40:11 +0900

    add akashic-library-version-checker

---
+++ build-template-support/checkAkashicVersions.js
@@ -0,0 +1,35 @@
+var path = require("path");
+var fs = require("fs");
+var targetVersions = ["v1", "v2"];
+var jsDirPath = path.resolve(__dirname + "/../js");
+targetVersions.forEach(function(version) {
+	console.log(version + ": start to check akashic-library-version");
+	var inputPackageJson =
+		JSON.parse(fs.readFileSync(path.join(__dirname, "..", "templates", "template-export-html-" + version, "package.json"), "utf8"));
+	var outputVersionsJson = JSON.parse(fs.readFileSync(path.join(__dirname, "..", "templates-build", version, "js", "versions.json"), "utf8"));
+	["dependencies", "devDependencies", "optionalDependencies"].forEach(function(item) {
+		if (inputPackageJson[item] === undefined) {
+			return;
+		}
+		for (var libName in inputPackageJson[item]) {
+			if (libName.match(/^@akashic/) === null) {
+				continue;
+			}
+			if (inputPackageJson[item][libName] !== outputVersionsJson[libName]) {
+				console.log(
+					"version of "
+					+ libName
+					+ " expect "
+					+ inputPackageJson[item][libName]
+					+ ", but current version is "
+					+ outputVersionsJson[libName]
+				);
+				throw new Error("current version is not expected");
+			} else {
+				console.log("OK: " + libName + "@" + inputPackageJson[item][libName]);
+			}
+		}
+	});
+	console.log(version + ": end to check akashic-library-version");
+});
+console.log("complete to check akashic-library-version");
---
+++ build-template-support/generateAkashicVersionsFile.js
@@ -0,0 +1,47 @@
+var path = require("path");
+var fs = require("fs");
+
+if (process.argv.length < 4) {
+	console.log("Please enter command as follows: node copyfiles.js version outputPath");
+	console.log("ex: node copyfiles.js v2 templates/template-export-html-v2/js");
+	throw new Error("invalid parameter");
+}
+
+// バージョン取得
+var version;
+switch (process.argv[2]) {
+	case "v1":
+	case "v2":
+		version = process.argv[2];
+		break;
+	default:
+		console.log("please designate version(v1 or v2)");
+		throw new Error("invalid parameter");
+}
+// 出力ファイルパス取得
+var outputPath = path.join(__dirname, "..", process.argv[3]);
+// ベースとするバージョン情報の取得
+var versions = {};
+if (typeof process.argv[4] !== "undefined") {
+	var baseFilePath = path.join(__dirname, "..", process.argv[4]);
+	fs.statSync(baseFilePath);
+	if (path.extname(baseFilePath) !== ".json") {
+		console.log("please specify json-file path");
+		throw new Error("invalid parameter");
+	}
+	versions = JSON.parse(fs.readFileSync(path.join(__dirname, "..", process.argv[4]), "utf8"));
+}
+
+var akashicLibDir = path.resolve(__dirname + "/../templates/template-export-html-" + version + "/node_modules/@akashic");
+var akashicLibs = fs.readdirSync(akashicLibDir);
+console.log("generate versions file about " + version);
+console.log("get vesion of each akashic-library");
+akashicLibs.forEach(function(libName) {
+	var packageJson = JSON.parse(fs.readFileSync(path.join(akashicLibDir, libName, "package.json"), "utf8"));
+	var keyName = "@akashic/" + libName;
+	versions[keyName] = packageJson["version"];
+	console.log(keyName + ":" + packageJson["version"]);
+});
+
+fs.writeFileSync(path.join(outputPath, "versions.json"), JSON.stringify(versions));
+console.log("saved vesion of each akashic-library");
