461a1645501f1ba03cf17046306a97d4c18252b0
dera- <alchemy.and.fairy@gmail.com>
2018-10-21 19:26:02 +0900

    reflect comment

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,7 +1,9 @@
 # CHANGELOG
 
 ## Unreleased changes
-* `--atsumaru` オプションの修正
+* `--atsumaru` オプション指定時、二度 lint される問題を修正
+* `--atsumaru` オプション指定時、`game.json` に `environment.niconico.supportedModes` が指定されているかのチェックを追加
+* export処理を開始したことを示す文言を表示
 
 ## 0.1.36
 * `--magnify`オプションを使用してもコンテンツがRPGアツマールページでリサイズされない不具合の修正
--- src/app/exportAtsumaru.ts
+++ src/app/exportAtsumaru.ts
@@ -34,7 +34,10 @@ export function promiseExportAtsumaru(param: ExportHTMLParameterObject): Promise
 			}
 			if (!gameJson.environment.niconico || !gameJson.environment.niconico.supportedModes) {
 				// モード指定がなければ、常に指定可能なモードであるsingleモードを追加する。
-				completedParam.logger.warn("'environment.niconico.supportedModes', a required property  for '--atsumaru' mode, is not given in game.json. Assumed to be [\"single\"].");
+				completedParam.logger.warn(
+					"'environment.niconico.supportedModes', a required property for '--atsumaru' mode," +
+					"is not given in game.json. Assumed to be [\"single\"]."
+				);
 				gameJson.environment.niconico = {
 					"supportedModes": ["single"]
 				};
